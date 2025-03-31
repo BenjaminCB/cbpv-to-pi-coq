be2035b5ce1a17d53d1b19d78a3e155a029ad34d
bcb <benjamincb@hotmail.dk>
2025-03-31 13:51:43 +0200

    save changes

--- project/bisimulation.v
+++ project/bisimulation.v
@@ -1,11 +1,59 @@
 From Encoding Require Export pi.
 
-Reserved Notation "P ~ Q" (at level 70).
+Definition bisimulation (R : proc -> proc -> Prop) : Prop :=
+  forall p q,
+    R p q ->
+    (forall l p',
+        p -( l )> p' ->
+        exists q',
+          q -( l )> q' /\
+          R p' q') /\
+    (forall l q',
+        q -( l )> q' ->
+        exists p',
+          p -( l )> p' /\
+          R p' q').
 
-Inductive bisimulation : (p q : proc) :=
-  | BI_IN1 : 
-    forall n m. p -( a_in n m )-> p' -> 
-      q -( a_in n m )-> q' /\ 
-      q' ~ p
+CoInductive bisimilar : proc -> proc -> Prop :=
+| bisim : forall p q,
+    (forall l p',
+        p -( l )> p' ->
+        exists q',
+          q -( l )> q' /\
+          bisimilar p' q') ->
+    (forall l q',
+        q -( l )> q' ->
+        exists p',
+          p -( l )> p' /\
+          bisimilar p' q') ->
+    bisimilar p q.
 
-  where "P ~ Q" := (bisimulation P Q).
+Definition weak_bisimulation (R : proc -> proc -> Prop) : Prop :=
+  forall p q,
+    R p q ->
+    (forall l p',
+        p -( l )> p' ->
+        exists q',
+          q =( l )> q' /\
+          R p' q') /\
+    (forall l q',
+        q -( l )> q' ->
+        exists p',
+          p =( l )> p' /\
+          R p' q').
+
+CoInductive weak_bisimilar : proc -> proc -> Prop :=
+| weak_bisim : forall p q,
+    (forall l p',
+        p -( l )> p' ->
+        exists q',
+          q =( l )> q' /\
+          weak_bisimilar p' q') ->
+    (forall l q',
+        q -( l )> q' ->
+        exists p',
+          p =( l )> p' /\
+          weak_bisimilar p' q') ->
+    weak_bisimilar p q.
+
+Notation "p ~~ q" := (weak_bisimilar p q) (at level 84).
