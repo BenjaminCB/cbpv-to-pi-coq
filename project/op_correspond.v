1f204903937668722df6b00c9eaebe777c7c613e
nikolajrk <nikolajrk@gmail.com>
2025-04-04 14:02:28 +0200

    Set up for the Jesper

--- project/op_correspond.v
+++ project/op_correspond.v
@@ -9,9 +9,20 @@ Require Coq.Lists.List.
 
 
 Theorem Sound_encoding : 
-forall (M N : term) (u r : nat), M --> N -> (exists (P' : proc), (encode M u r List.nil) =(a_tau)> P' /\ (P' ~~ (encode N u r List.nil))). 
+forall (M N : term) (u r : nat), M --> N -> (exists (P' : proc), (encode M u r List.nil) =(a_tau)> P' 
+  /\ (P' ~~ (encode N u r List.nil))). 
 Proof. intros. induction H.
   - simpl.
   induction v.
   * simpl.
   Admitted.
+
+Theorem Complete_encoding : 
+forall (M : term) (P : proc) (u r : nat), (encode M u r List.nil) -(a_tau)> P 
+  -> (exists (P' : proc) (N : term),  P =(a_tau)> P' 
+  /\ (P' ~~ (encode N u r List.nil)) /\ (M --> N \/ M = N)).
+Proof. intro. induction M. intros.
+  - induction H. 
+  *
+
+
