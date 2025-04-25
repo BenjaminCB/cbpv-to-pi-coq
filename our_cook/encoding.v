2ee0f7596733a9a568cc5e05727434304a2390d2
BCB <benjamincb@hotmail.dk>
2025-04-25 13:55:25 +0200

    fix encoding

--- our_cook/encoding.v
+++ our_cook/encoding.v
@@ -1,55 +1,68 @@
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Nat.
 From Encoding Require Export cbpv.
 From Encoding Require Export pi.
-Require Coq.Lists.List.
-Require Import Nat.
 
-Definition incProduct (n : nat) (m : nat) (pair : nat * nat) := 
-  match pair with
-  | (n', m') => (n + n', m + m')
+Fixpoint incRefs (n : nat) (m : nat) (refs : list (nat * nat)) :=
+  match refs with
+  | [] => []
+  | (n', m') :: ps => (n + n', m + m') :: (incRefs n m ps)
   end.
 
-Fixpoint incRefs (n : nat) (m : nat) (refs : List.list (nat * nat)) :=
+Fixpoint findRef (n : nat) (refs : list (nat * nat)) :=
   match refs with
-  | List.nil => List.nil
-  | List.cons p ps => List.cons 
-      (incProduct n m p)
-      (incRefs n m ps)
-  end.
-
-Fixpoint findRef (n : nat) (refs : List.list (nat * nat)) :=
-  match refs with
-  | List.nil => n 
-  | List.cons (x, y) ps => 
+  | [] => n 
+  | (x, y) :: ps => 
       if n =? x 
       then y
       else findRef n ps
   end.
 
-Fixpoint encode (s : term) (u r : nat) (refs : List.list (nat * nat)) :=
+Fixpoint applier (constructor : proc -> proc) (n : nat) (p : proc) :=
+  match n with
+  | 0 => p
+  | S n => constructor (applier constructor n p)
+  end.
+  
+Notation "constructor ^^ n" := (applier constructor n) (at level 90, left associativity).
+
+Fixpoint encode (s : term) (u r : nat) (refs : list (nat * nat)) :=
   match s with
-  | Val v => Res (Out (u+1) 0 (encode_value v (incRefs 0 1 refs)))
-  | Abs s => In u (In 0 (In 1 (In 2 (encode s 2 1 (List.cons (0,0) (incRefs 1 4 (refs)))))))
-  | App s v => Res (Res (Par 
-      (encode s 1 0 (incRefs 0 2 refs)) 
-      (Res (Out 2 0 (Out 0 (u + 3) (Out 0 (r + 3) 
-        ( ( Res (Out (u+4) 0 (encode_value v (incRefs 0 4 refs) ))))
-      ))))
-    ))
-  | Force v => Res (Par 
-      (Res ( Out (1) 0(encode_value v (incRefs 0 1 refs)) ))
-      (In 0 (Res (Out 1 0 (Out 0 (u + 3) (Out 0 (r + 3) (Nil))))))
+  | Abs s => Out u (In 0 (In 1 (In 2 (encode s 2 1 ((0,0) :: incRefs 1 4 refs)))))
+  | App s v => (Res ^^ 4) (Par 
+      (In 3 (In 2 (Out 1 (Out 2 (Out 3 (Par
+        (Link 2 (u + 9))
+        (Par
+          (Link 1 (r + 9))
+          (Link 0 3)
+        )
+      ))))))
+      (Par
+        (encode s 3 2 (incRefs 0 4 refs))
+        (encode_value v 1 0 (incRefs 0 4 refs))
+      )
     )
-  | Ret v => (Res (Out (r+1) 0 (encode_value v (incRefs 0 1 refs))))
-  | Bind s t => Res (Par 
-      (Res (encode s 0 1 (incRefs 0 2 refs))) 
-      (In 0 (encode t (u + 2) (r + 2) (List.cons (0,0)(incRefs 1 2 refs))))
+  | Force v => (Res ^^ 2) (Par
+      (encode_value v 0 1 (incRefs 0 2 refs))
+      (In 1 (Out 0 (Out 0 (Out 1 (Par
+        (Link 1 (u + 6))
+        (Link 0 (r + 6))
+      )))))
     )
+  | Ret v => encode_value v r r refs
+  | Bind s t => (Res ^^ 2) (Par
+      (encode s 1 0 (incRefs 0 2 refs))
+      (In 0 (encode t (u + 3) (r + 3) (incRefs 1 3 refs)))
+    )
+  | Val v => encode_value v u r refs
   end
 
-with encode_value (v : value) (refs : List.list (nat * nat)) :=
+with encode_value (v : value) (u r : nat) (refs : list (nat * nat)) :=
   match v with
-  | Var n => Rep (In 0 (Out ((findRef n refs) + 1) 0 Nil))
-  | Thunk s => (Rep (In 0 (In 0 (In 1 
-      (encode s 1 0 (incRefs 0 3 refs))
-    ))))
+  | Var n => Out u (Rep (In 0 (In 0 (In 1 (In (n + 4) (In 0 (In 1 (Par
+      (Link 1 4)
+      (Link 0 3)
+    ))))))))
+  | Thunk s => Out u (Rep (In 0 (In 0 (In 1 (encode s 1 0 (incRefs 0 4 refs))))))
   end.
