54cf51b80f5303b745bc072738699443d489309c
BCB <benjamincb@hotmail.dk>
2025-04-26 22:34:17 +0200

    work on proof

--- our_cook/op_correspond.v
+++ our_cook/op_correspond.v
@@ -1,5 +1,6 @@
 Require Import Coq.Lists.List.
 Import ListNotations.
+Require Import Coq.Relations.Relation_Operators. 
 From Encoding Require Export cbpv.
 From Encoding Require Export pi.
 From Encoding Require Export encoding.
@@ -10,6 +11,29 @@ Theorem sound: forall s t,
     exists P,
       (encode s u r []) =()> P /\ (P ~~ (encode t u r [])).
 Proof.
+  intros s t Hred.
+  induction Hred.
+  - admit.
+  - admit.
+  - intros u r.
+    exists (encode s u r []).
+    split.
+    * eapply rt_trans.
+      + apply rt_step.
+        simpl.
+        apply RES_TAU.
+        apply RES_TAU.
+        { apply COM with
+            (a := a_out 0)
+            (R := Rep (In 0 (In 0 (In 1 (encode s 1 0 [])))))
+            (S := Out 0 (Out 0 (Out 1 (Par (Link 1 (u + 6)) (Link 0 (r + 6)))))).
+          - exact 0.
+          - discriminate.
+          - admit.
+          - simpl.
+        }  
+        
+    * apply wb_ref.
 Admitted.
 
 Theorem complete: forall s P u r, 
